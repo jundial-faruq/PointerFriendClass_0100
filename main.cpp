2926844ba74b0fe2c4b5239a4a8450f383cf713f
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-05-06 14:56:27 +0700

    adding member ke lingkaran class

--- main.cpp
+++ main.cpp
@@ -18,7 +18,27 @@ public:
     {
         return a + b;
     }
+
+    Lingkaran ol;
 };
+
+class Lingkaran
+{
+    public:
+    int jarijari;
+
+    void input()
+    {
+        cout << "Masukan jari-jari : ";
+        cin >> jarijari;
+    }
+
+    int LuasLingkaran(int r)
+    {
+        return 3,14 + r * r;
+    }
+};
+
 
 int main() {
 
