32052c32d605245414ba2619ef3d1fd2f9e4b3b0
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-05-06 14:49:59 +0700

    adding member to persegipanjang class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+class PersegiPanjang {
+public: // akses modifier
+    int panjang, lebar;
+
+public: 
+    void input()
+    {
+        cout << "Masukan Panjang : ";
+        cin >> panjang;
+        cout << "Masukan Lebar : ";
+        cin >> lebar;
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a + b;
+    }
+};
 
 int main() {
 
